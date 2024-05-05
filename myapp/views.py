c7396fbcf5c2b6159419f77e21177dfba2c04871
Jess Moore <104534587+jmychalm13@users.noreply.github.com>
2024-05-05 17:13:18 -0500

    added Property model and migrated to DB

--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from pandas import pandas
+import json
 
 
 # Create your views here.
@@ -8,6 +9,10 @@ def hello(request):
     if request.method == "POST":
         file = request.FILES["file"]
         df = pandas.read_csv(file)
+        json_records = df.reset_index().to_json(orient="records")
+        data = []
+        data = json.loads(json_records)
+        print(data)
     else:
         print("This is a GET request")
     return render(request, "myapp/index.html")
