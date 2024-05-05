2acbb445a5c86de552fae2928842405df2f999e6
Jess Moore <104534587+jmychalm13@users.noreply.github.com>
2024-05-05 16:47:15 -0500

    installed pandas and getting file from form

--- myapp/views.py
+++ myapp/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from pandas import pandas
 
 
 # Create your views here.
 def hello(request):
+    if request.method == "POST":
+        file = request.FILES["file"]
+        df = pandas.read_csv(file)
+    else:
+        print("This is a GET request")
     return render(request, "myapp/index.html")
